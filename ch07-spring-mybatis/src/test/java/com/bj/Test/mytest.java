af1e4b9e113e42769263ea36857ea8503d29aa6b
zdp <1263214893@qq.com>
2020-11-16 17:32:30 +0800

    1

--- ch07-spring-mybatis/src/test/java/com/bj/Test/mytest.java
+++ ch07-spring-mybatis/src/test/java/com/bj/Test/mytest.java
@@ -2,10 +2,12 @@ package com.bj.Test;
 
 import com.bj.dao.StudentDao;
 import com.bj.domain.Student;
+import com.bj.service.StudentService;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import javax.xml.ws.Service;
 import java.util.List;
 
 public class mytest {
@@ -33,6 +35,22 @@ public class mytest {
         student.setId(1012);
         student.setName("黑马");
         int n = studentDao.insertStudent(student);
+        System.out.println(n);
+
+    }
+
+    @Test
+    public void Servicetest03() {
+        String config = "SpringApplicationContext.xml";
+        ApplicationContext ac = new ClassPathXmlApplicationContext(config);
+        //获取spring容器中的dao对象
+        StudentService studentService = (StudentService) ac.getBean("studentService");
+        Student student = new Student();
+        student.setAge(12);
+        student.setEmail("gfdsafga");
+        student.setId(1012);
+        student.setName("李圣卢");
+        int n = studentService.addStudent(student);
         System.out.println(n);
 
     }
