6b3a181535277beaf49c9e67f46a5c14bb235a4d
zdp <1263214893@qq.com>
2020-11-30 17:30:27 +0800

    1

---
+++ leyou/leyou-item/leyou-item-service/src/main/java/com/leyou/service/CategoryService.java
@@ -0,0 +1,22 @@
+package com.leyou.service;
+
+import com.leyou.mapper.CategoryMapper;
+import com.leyou.pojo.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryMapper categoryMapper;
+
+    public List<Category> queryCategoriesByPid(Long pid) {
+        Category record = new Category();
+        record.setParentId(pid);
+        return this.categoryMapper.select(record);
+    }
+
+}
